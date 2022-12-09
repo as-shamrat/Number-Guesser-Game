fa9f5dfa9ed99851a3d45447cd1b50e7d5d91595
as-shamrat <67526675+as-shamrat@users.noreply.github.com>
2022-12-10 00:16:10 +0600

    third commit

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ let low = 1;
 let high = 10;
 let correct_ans = Math.floor(Math.random() * (high - low) + low);
 let notMatched = true;
+for (let i = 1; i <= 3; i++) {
+  let userInput = Number(prompt(`Enter a number between (${low}-${high}):`));
+  if (correct_ans > userInput) {
+    alert("correct answer is greater!");
+  } else if (correct_ans < userInput) {
+    alert("corrent answer is smaller!");
+  } else {
+    alert("you win!!!");
+    notMatched = false;
+    break;
+  }
+}
